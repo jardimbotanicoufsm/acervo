0bb221e65a83b20d3d3ef33c276a8c133a3f8102
Samuel Steffler <samuel.steffler@acad.ufsm.br>
2025-05-19 19:17:05 -0300

    finally fixed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,12 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/acervo/',
   plugins: [
     vue(),
     vueDevTools(),
   ],
   resolve: {
-    base: '/acervo/',
     server: {
       historyApiFallback: true, // This will handle route fallbacks for history mode
     }
