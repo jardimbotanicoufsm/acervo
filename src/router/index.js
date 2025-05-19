64e56a5ed2ef04380de6cf4a615806775e34fdb9
Samuel Steffler <samuel.steffler@acad.ufsm.br>
2025-05-19 10:47:10 -0300

    updated router.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router';
 import PlantDetail from '../views/PlantDetail.vue';
 
 const routes = [
-  { path: '/acervo/item/:id', name: 'PlantDetail', component: PlantDetail },
-  { path:'/acervo', name: 'Acervo', component: () => import('../views/PlantCollection.vue') },
+  { path: '/item/:id', name: 'PlantDetail', component: PlantDetail },
+  { path:'/', name: 'Acervo', component: () => import('../views/PlantCollection.vue') },
 ];
 
 export default createRouter({
-  history: createWebHistory('/jb-pages/'),
+  history: createWebHistory('/acervo/'),
   routes,
 });
