564fd989d202f91efdbc27a62d2cfbc16674740d
Samuel Steffler <samuel.steffler@acad.ufsm.br>
2025-05-20 21:32:21 -0300

    Updated header sizing to small screens
    Co-authored-by: Tiago Steffler <tiago.steffler@acad.ufsm.br>

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <header>
-      <div class="header">
-        <div class="header-left">
-            <img :src="logoSrc" alt="Logo do Jardim Botânico" class="logo" style="width: 50px; height: 50px;" />
-          <div class="header-title">
-            <div style="display: flex; gap: 10px; align-items: center;">
-              <h1 class="myriad-pro-regular">Jardim Botânico</h1>
-              <h1 class="zapf-humnst-regular">UFSM</h1>
-            </div>
+    <div class="header-left">
+      <div class="header-logo">
+        <img :src="logoSrc" alt="Logo do Jardim Botânico" class="logo" style="width: 50px; height: 50px;" />
+        <div class="header-title">
+          <div style="display: flex; gap: 10px; align-items: center;">
+            <h1 class="myriad-pro-regular">Jardim Botânico</h1>
+            <h1 class="zapf-humnst-regular">UFSM</h1>
           </div>
         </div>
       </div>
-      <nav>
+    </div>
+    <div class="header-right">
+      <nav class="main-nav">
         <router-link to="/sobre">Sobre</router-link>
         <router-link to="/atracoes">Atrações</router-link>
         <router-link to="/visite">Visite</router-link>
@@ -19,12 +20,15 @@
         <router-link to="/itens">Acervo</router-link>
         <router-link to="/mapa">Mapa</router-link>
       </nav>
-    </header>
+    </div>
+  </header>
+
   <router-view />
   <footer>
       &copy; 2025 Jardim Botânico - Todos os direitos reservados
   </footer>
 </template>
+
 
 <script>
 import logo from '@/assets/img/logo1.png'
